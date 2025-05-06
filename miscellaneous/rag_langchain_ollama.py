1493f6a3d589a68f0d67e6f51e52679558f4f983
nmthang6 <nmthang6@cmcglobal.vn>
2025-05-06 18:08:17 +0700

    feat: test rag with ollama

---
+++ miscellaneous/rag_langchain_ollama.py
@@ -0,0 +1,36 @@
+from langchain_community.document_loaders import TextLoader
+
+
+class QueryTranslation:
+    def __init__(self):
+        pass
+
+
+class Routing:
+    def __init__(self):
+        pass
+
+
+class QueryConstruction:
+    def __init__(self):
+        pass
+
+
+class Indexing:
+    def __init__(self):
+        pass
+
+
+class Retrieval:
+    def __init__(self):
+        pass
+
+
+class Generation:
+    def __init__(self):
+        pass
+
+
+class App:
+    def __init__(self):
+        pass
